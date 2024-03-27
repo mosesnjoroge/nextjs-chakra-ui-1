42c1e5c935b5aff673af37722953c86770e60ff5
mosesnjoroge <mosesbnjoroge@outlook.com>
2024-03-27 14:18:38 -0400

    imported vstack and flex component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
+import { Container, Flex, VStack } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <>
-      <h1>Hello Next.js</h1>
-
-    </>
+    <Container maxW="container.xl" p={0}>
+      <Flex h="100vh" py={20}>
+        <VStack
+          w="full"
+          h="full"
+          spacing={10}
+          alignItems="flex-start"
+        >
+          <h1>Hello Next.js</h1>
+          <h1>Hello Next.js</h1>
+        </VStack>
+        <VStack
+          w="full"
+          h="full"
+          spacing={10}
+          alignItems="flex-start"
+          bg="gray.50"
+        >
+          <h1>Hello Next.js</h1>
+          <h1>Hello Next.js</h1>
+        </VStack>
+      </Flex>
+    </Container>
   );
 }
